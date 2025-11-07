74b34cb1e1b3a6377fcae13bdf4bcbcc3c715630
Vishwajeet-003 <vishmandal2003@gmail.com>
2025-11-07 20:09:15 +0530

    Fix Vercel deployment configuration

--- api/app.py
+++ api/app.py
@@ -1,4 +1,8 @@
-from app import app
+from flask import Flask
+from app import app as flask_app
 
 # This is the Vercel entrypoint
-app = app
+app = flask_app
+
+if __name__ == '__main__':
+    app.run()
